75fe36eba70050a0b039c9047032b1ad9ad752eb
itzzuy <duythe0210@gmail.com>
2026-02-11 16:22:23 -0500

    database system design

---
+++ backend2gether/prisma/seed.js
@@ -0,0 +1,10 @@
+import { PrismaClient } from "@prisma/client"
+
+const prisma = new PrismaClient();
+
+const cretorId = "419a45ae-5dfa-49f0-b434-d9db1222fb67"
+
+
+const main = async () => {
+  console.log("Seeding ")
+}
--- backend2gether/src/controllers/authController.js
+++ backend2gether/src/controllers/authController.js
@@ -26,7 +26,7 @@ const register = async (req, res) => {
     },
   });
 
-  const token = generateToken(user.id)
+  const token = generateToken(user.id, res)
 
   res.status(201).json({
     status: "success",
@@ -60,7 +60,7 @@ const login = async (req, res) => {
   }
 
   // Generate JWT Token
-  const token = generateToken(user.id)
+  const token = generateToken(user.id, res)
 
   res.status(201).json({
     status: "success",
@@ -72,6 +72,14 @@ const login = async (req, res) => {
       token,
     },
   });
+};
+
+const logout = async (req, res) => {
+  res.cookie("jwt", "", {
+    httpOnly: true,
+    expires: new Date(0)
+  })
+  res.status(200).json({ status: "success", message: "Logged out successfully"})
 }
 
-export {register, login}
+export {register, login, logout}
